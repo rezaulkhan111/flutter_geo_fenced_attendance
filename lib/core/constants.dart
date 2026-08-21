70403027c51b010e7fe2a34002c7501ae42f5065
rezaulkhan111 <rezaulkhan111@hotmail.com>
2026-08-22 01:35:01 +0600

    .

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const double officeRadiusThreshold = 10.0; // meters
+  static const double officeRadiusThreshold = 50.0; // meters
   static const String officeLatitudeKey = 'office_latitude';
   static const String officeLongitudeKey = 'office_longitude';
 }
